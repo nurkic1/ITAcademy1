b08525766e8320fec42c572e4d5945ac7ea60488
mnurkiczzz <nurkicmehmed05@gmail.com>
2023-01-24 17:30:48 +0100

    Zavrseno

---
+++ Advanced Java Programming/src/je.java
@@ -0,0 +1,11 @@
+public class je {
+    public static void main(String[] args) {
+        int height = 112;
+        int radius = 45;
+        double pie = 3.142857;
+        double volume = pie*(radius*radius)*height;
+        System.out.println("Volume of the cylinder ="+volume);
+
+        }
+    }
+
---
+++ Vjezbe za akademiju/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int height = 112;
+        int radius = 45;
+        double pie = 3.142857;
+        double volume = pie*(radius*radius)*height;
+        System.out.println("Volume of the cylinder ="+volume);
+
+    }
+}
+
+
+
